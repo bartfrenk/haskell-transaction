9a1a1bd90dc865f78f3e1fca78aa74440256fef0
Bart Frenk <bart.frenk@gmail.com>
2016-02-29 14:07:37 +0100

    IN PROGRESS: integrate AppMonad

--- app/GUI.hs
+++ app/GUI.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# OPTIONS_GHC -fno-warn-unused-do-bind #-}
 import Transaction
 
@@ -5,9 +6,23 @@ import Utils (loadTransactionFiles, gather)
 import Graphics.UI.Gtk
 import Graphics.UI.Gtk.ModelView as New
 import Control.Monad.Trans (liftIO)
-import Data.Map.Lazy (toList, keys)
+import Data.Map.Lazy (keys)
+import Control.Monad.Reader
+import Control.Monad.State
 
-type GUIState = ([Transaction], [Account])
+data AppState = AppState {
+  stTransactions :: [Transaction],
+  stAccounts :: [Account]
+  }
+
+data AppConfig = AppConfig {
+  cfgGladePath :: FilePath
+  }
+
+newtype AppMonad a = AppMonad {
+  getApp :: ReaderT AppConfig (StateT AppState IO) a
+  } deriving (Functor, Applicative, Monad, MonadIO,
+              MonadReader AppConfig, MonadState AppState)
 
 data MainWindow = MainWindow {
   appWindow :: Window,
@@ -25,7 +40,8 @@ data OpenDialog = OpenDialog {
 
 data GUI = GUI {
   mainWindow :: MainWindow,
-  openDialog :: OpenDialog
+  openDialog :: OpenDialog,
+  accountStore :: ListStore Account
   }
 
 showOpenDialog :: OpenDialog -> IO ()
@@ -52,7 +68,8 @@ loadGUI :: FilePath -> IO GUI
 loadGUI gladePath = do
   builder <- createBuilder gladePath
   GUI <$> loadMainWindow builder <*>
-          loadOpenDialog builder
+          loadOpenDialog builder <*>
+          New.listStoreNew []
 
 createBuilder :: FilePath -> IO Builder
 createBuilder gladePath = do
@@ -61,8 +78,14 @@ createBuilder gladePath = do
   return builder
 
 -- |Connects handlers for signals.
+connectGUI :: GUI -> IO ()
+connectGUI gui = do
+  connectMainWindow gui
+  connectOpenDialog gui
+  connectAccountPane gui
+
 connectMainWindow :: GUI -> IO ()
-connectMainWindow (GUI mainWin openDlg) = do
+connectMainWindow (GUI mainWin openDlg _) = do
   appWindow mainWin `on` deleteEvent $ liftIO mainQuit >> return False
   quitMenuItem mainWin `on` menuItemActivated $ liftIO mainQuit
   openMenuItem mainWin `on` menuItemActivated $ showOpenDialog openDlg
@@ -72,38 +95,41 @@ connectOpenDialog :: GUI -> IO ()
 connectOpenDialog gui = do
   cancelBtn openDlg `on` buttonActivated $ (widgetHide dlg)
   openBtn openDlg `on` buttonActivated $ do
-    state <- fileChooserGetFilenames dlg >>= openFiles
-    redraw gui state
+    st <- fileChooserGetFilenames dlg >>= openFiles
+    redraw gui st
     widgetHide dlg
   return ()
   where openDlg = openDialog gui
         dlg = chooserDlg openDlg
 
-openFiles :: [FilePath] -> IO GUIState
-openFiles paths = do
-  ts <- loadTransactionFiles paths
-  let accounts = gather trDest trAmount ts
-  mapM_ putStrLn (keys accounts)
-  return (ts, keys accounts)
-
-connectGUI :: GUI -> IO ()
-connectGUI gui = do
-  connectMainWindow gui
-  connectOpenDialog gui
-
-redraw :: GUI -> GUIState -> IO ()
-redraw gui state = do
-  store <- New.listStoreNew (snd state)
-  treeViewSetModel pane store
+connectAccountPane :: GUI -> IO ()
+connectAccountPane gui = do
   renderer <- New.cellRendererTextNew
   column <- New.treeViewColumnNew
   New.treeViewColumnPackStart column renderer True
+  treeViewSetModel pane store
   New.cellLayoutSetAttributes column renderer store $ \row -> [New.cellText := row]
   New.treeViewColumnSetTitle column "Account"
   New.treeViewAppendColumn pane column
   return ()
   where pane = accountsPane (mainWindow gui)
+        store = accountStore gui
 
+openFiles :: [FilePath] -> AppMonad ()
+openFiles paths = do
+  ts <- liftIO $ loadTransactionFiles paths
+  let accounts = gather trDest trAmount ts
+  put (AppState ts (keys accounts))
+  return ()
+
+initialState :: IO (ListStore Account)
+initialState = New.listStoreNew []
+
+redraw :: GUI -> AppState -> IO ()
+redraw gui st = do
+  listStoreClear store
+  forM_ (stAccounts st) (listStoreAppend store)
+  where store = accountStore gui
 
 main :: IO ()
 main = do
